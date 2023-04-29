f12d82ab2c3c3e3b221c9cd60a93bdd87ad907c8
Milo-Shen <Milo-Shen@outlook.com>
2023-04-29 22:25:45 +0800

    feat: render card in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // Import React Framework
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // Import Customized Component
 import SimpleOrgChart from "./Component/SimpleOrgChart/SimpleOrgChart";
@@ -8,20 +8,25 @@ import SimpleOrgChart from "./Component/SimpleOrgChart/SimpleOrgChart";
 import "./App.css";
 
 // Import Utils
+import { CardNode, OrgChart } from "./OrgChart/OrgChart";
 import { mock_org_chart_data } from "./Utils/mock_org_chart_data";
-import { OrgChart } from "./OrgChart/OrgChart";
-
-let now = performance.now();
-let data = mock_org_chart_data(100, 5);
-let chart = new OrgChart(data);
-let render_data = chart.get_render_data();
-console.log(`${performance.now() - now} ms`);
-console.log(chart, render_data);
 
 function App() {
+  let [card_list, set_card_list] = useState<Array<Array<CardNode>>>([]);
+
+  useEffect(() => {
+    let now = performance.now();
+    let data = mock_org_chart_data(100, 5);
+    let chart = new OrgChart(data);
+    let render_data = chart.get_render_data();
+    set_card_list(render_data.render_list);
+    console.log(`${performance.now() - now} ms`);
+    console.log(chart, render_data);
+  }, []);
+
   return (
     <div className="App">
-      <SimpleOrgChart />
+      {card_list.map((level) => level.map((card) => <SimpleOrgChart />))}
     </div>
   );
 }
