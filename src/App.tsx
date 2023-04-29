246ad9db077718a92294e992bb6f3d60160072e9
Milo-Shen <Milo-Shen@outlook.com>
2023-04-29 16:40:10 +0800

    feat: add mock_org_chart_data

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Import Utils
+import { mock_org_chart_data } from "./Utils/mock_org_chart_data";
+// mock_org_chart_data(100);
+
 function App() {
   return (
     <div className="App">
--- src/Utils/mock_org_chart_data.ts
+++ src/Utils/mock_org_chart_data.ts
@@ -1,3 +1,45 @@
-export function mock_org_chart_data(count: number = 1) {}
+// Import Utils
+import { nanoid } from "nanoid";
 
-mock_org_chart_data();
+function build_card(): { id: string; child: any[] } {
+  return {
+    id: nanoid(),
+    child: [],
+  };
+}
+
+export function mock_org_chart_data(count: number = 1, max_child?: number) {
+  max_child = max_child || Math.sqrt(count);
+  let result = [];
+  let queue = [];
+
+  // generated leaf count
+  let generated_total = 1;
+
+  // build the root leaf
+  let root = build_card();
+
+  queue.push(root);
+
+  while (queue.length) {
+    let node = queue.shift();
+    let temp: any[] = [];
+
+    for (let i = 0; i < max_child; i++) {
+      generated_total++;
+      let card = build_card();
+      queue.push(card);
+      result.push(card);
+    }
+
+    node!.child = temp;
+
+    if (generated_total >= count) {
+      return result;
+    }
+  }
+
+  result.push(root);
+
+  return result;
+}
