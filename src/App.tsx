9cb84079055ff9d3f9b5959a485841350e880b81
Milo-Shen <Milo-Shen@outlook.com>
2023-04-29 22:39:09 +0800

    feat: update get_render_data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // Import React Framework
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 // Import Customized Component
 import SimpleOrgChart from "./Component/SimpleOrgChart/SimpleOrgChart";
@@ -12,9 +12,15 @@ import { CardNode, OrgChart } from "./OrgChart/OrgChart";
 import { mock_org_chart_data } from "./Utils/mock_org_chart_data";
 
 function App() {
+  let is_fetch = useRef(false);
+
   let [card_list, set_card_list] = useState<Array<Array<CardNode>>>([]);
 
   useEffect(() => {
+    if (is_fetch.current) {
+      return;
+    }
+
     let now = performance.now();
     let data = mock_org_chart_data(100, 5);
     let chart = new OrgChart(data);
@@ -22,11 +28,24 @@ function App() {
     set_card_list(render_data.render_list);
     console.log(`${performance.now() - now} ms`);
     console.log(chart, render_data);
+
+    return () => {
+      is_fetch.current = true;
+    };
   }, []);
 
   return (
     <div className="App">
-      {card_list.map((level) => level.map((card) => <SimpleOrgChart />))}
+      {card_list.map((level) =>
+        level.map((card) => (
+          <SimpleOrgChart
+            key={card.id}
+            name={card.name}
+            pos_x={card.pos_x}
+            pos_y={card.pos_y}
+          />
+        ))
+      )}
     </div>
   );
 }
--- src/Component/SimpleOrgChart/SimpleOrgChart.tsx
+++ src/Component/SimpleOrgChart/SimpleOrgChart.tsx
@@ -4,8 +4,16 @@ import React from "react";
 // Import CSS
 import OrgChartStyle from "./SimpleOrgChart.module.css";
 
-function OrgChart() {
-  return <div className={OrgChartStyle.simple_org_chart}>OrgChart</div>;
+function OrgChart(props: any) {
+  const { name, pos_x, pos_y } = props;
+  return (
+    <div
+      style={{ left: pos_x, top: pos_y }}
+      className={OrgChartStyle.simple_org_chart}
+    >
+      {name}
+    </div>
+  );
 }
 
 export default OrgChart;
