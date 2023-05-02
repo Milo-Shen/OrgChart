f4a54259328596543f6aa494694cd86ea9b3cafb
Milo-Shen <Milo-Shen@outlook.com>
2023-05-02 20:15:09 +0800

    feat: update the test data

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,12 @@ function App() {
       return;
     }
 
+    // todo: test it
     let now = performance.now();
-    let data = mock_org_chart_data(15, 5, false);
+    let data = mock_org_chart_data(11, 3, false);
+    data[7].children.push("id=11");
+    data.push({ children: ["id=12"], id: "id=11", name: "id=11" });
+    data.push({ children: [], id: "id=12", name: "id=12" });
     let chart = new OrgChart(data);
     let render_data = chart.get_render_data();
     set_card_list(render_data);
