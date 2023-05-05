d15258d7ae29fd769cd6e0080f1892df7e43e1cb
Milo-Shen <Milo-Shen@outlook.com>
2023-05-05 14:05:04 +0800

    feat: add chart component

---
+++ src/Component/Chart/Chart.tsx
@@ -0,0 +1,11 @@
+// Import React Framework
+import React from "react";
+
+// Import CSS
+
+function Chart(props: any) {
+  const { children } = props;
+  return <div></div>;
+}
+
+export default Chart;
