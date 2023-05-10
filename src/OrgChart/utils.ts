d4c1b4f8a6ef31931be5ea64f259fe6022db91e8
Milo-Shen <Milo-Shen@outlook.com>
2023-05-10 08:13:59 +0800

    feat: update the comment of Comparator

--- src/OrgChart/utils.ts
+++ src/OrgChart/utils.ts
@@ -28,21 +28,11 @@ export function traverse_tree(
 
 export default class Comparator {
   compare: Function;
-  /**
-   * Constructor.
-   * @param {function(a: *, b: *)} [compareFunction] - It may be custom compare function that, let's
-   * say may compare custom objects together.
-   */
+
   constructor(compareFunction: Function) {
     this.compare = compareFunction || Comparator.defaultCompareFunction;
   }
 
-  /**
-   * Default comparison function. It just assumes that "a" and "b" are strings or numbers.
-   * @param {(string|number)} a
-   * @param {(string|number)} b
-   * @returns {number}
-   */
   static defaultCompareFunction(a: number, b: number) {
     if (a === b) {
       return 0;
@@ -51,59 +41,26 @@ export default class Comparator {
     return a < b ? -1 : 1;
   }
 
-  /**
-   * Checks if two variables are equal.
-   * @param {*} a
-   * @param {*} b
-   * @return {boolean}
-   */
   equal(a: number, b: number) {
     return this.compare(a, b) === 0;
   }
 
-  /**
-   * Checks if variable "a" is less than "b".
-   * @param {*} a
-   * @param {*} b
-   * @return {boolean}
-   */
   lessThan(a: number, b: number) {
     return this.compare(a, b) < 0;
   }
 
-  /**
-   * Checks if variable "a" is greater than "b".
-   * @param {*} a
-   * @param {*} b
-   * @return {boolean}
-   */
   greaterThan(a: number, b: number) {
     return this.compare(a, b) > 0;
   }
 
-  /**
-   * Checks if variable "a" is less than or equal to "b".
-   * @param {*} a
-   * @param {*} b
-   * @return {boolean}
-   */
   lessThanOrEqual(a: number, b: number) {
     return this.lessThan(a, b) || this.equal(a, b);
   }
 
-  /**
-   * Checks if variable "a" is greater than or equal to "b".
-   * @param {*} a
-   * @param {*} b
-   * @return {boolean}
-   */
   greaterThanOrEqual(a: number, b: number) {
     return this.greaterThan(a, b) || this.equal(a, b);
   }
 
-  /**
-   * Reverses the comparison order.
-   */
   reverse() {
     const compareOriginal = this.compare;
     this.compare = (a: number, b: number) => compareOriginal(b, a);
