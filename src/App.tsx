800e894831567b1d34cb02f8a0ed2c539d0ccf06
Milo-Shen <Milo-Shen@outlook.com>
2023-05-14 20:00:34 +0800

    feat: update src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,14 +35,14 @@ function App() {
     //   ~~(Math.random() * 5) + 1,
     //   true
     // );
-    let data = mock_org_chart_data(10, 2, false);
+    let data = mock_org_chart_data(10, 3, false);
     // data[7].children.push("id=11");
     // data.push({ children: ["id=12"], id: "id=11", name: "id=11" });
     // data.push({ children: [], id: "id=12", name: "id=12" });
     let chart = new OrgChart(data, true, 200, 100, 10, 41, 2, "#6A6D70", 12);
     let render_data = chart.get_render_data();
-    set_card_list(render_data);
     console.log(`${performance.now() - now} ms`);
+    set_card_list(render_data);
     console.log(chart);
     console.log(render_data);
 
