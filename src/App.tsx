a4bbebd0f63635d754889c64d617d3ab4e1cdf71
Milo-Shen <Milo-Shen@outlook.com>
2023-12-03 13:36:35 +0800

    feat: update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import SimpleOrgChart from "./Component/SimpleOrgChart";
 
 // Import Utils
 import { CardNode, OrgChart, chartRenderDefaultData } from "./OrgChart/OrgChart";
-import { mock_org_chart_data } from "./Utils/mock_org_chart_data";
+// import { mock_org_chart_data } from "./Utils/mock_org_chart_data";
 
 function App() {
   let is_fetch = useRef(false);
